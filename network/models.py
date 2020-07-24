072cba9ab32a89923dc8fd1325c2e1d9421d86eb
Nyasha Vision <nmawungwe@gmail.com>
2020-07-24 21:36:48 +0200

    working on post model

--- network/models.py
+++ network/models.py
@@ -4,3 +4,16 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    sender = models.ForeignKey("User", on_delete=models.PROTECT)
+    body = models.TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.PositiveIntegerField()
+
+
+
+
+
