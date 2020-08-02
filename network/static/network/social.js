ba1d8f69bb4e7d151bb6b2114a968b772d17108f
Nyasha Vision <nmawungwe@gmail.com>
2020-08-02 21:58:24 +0200

    Added dataset extraction....had some trouble initially

--- network/static/network/social.js
+++ network/static/network/social.js
@@ -1,17 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 document.querySelector('#user_prof').addEventListener('click',()=>{
-    fetch(`/user_profile/3`).then(response => response.json()).then(user_prof => {
+    const el = document.querySelector('#user_prof')
+        // console.log('clicking')
+
+        user_id = el.dataset.user
+        console.log(user_id) 
+
+    
+    
+    user_profile(fetch(`/user_profile/${user_id}`).then(response => response.json()).then(user_prof => {
         // Print email
         console.log(user_prof)
-    })
+    }))
+    
+
+
 })
 
-document.querySelector('#all_posts').addEventListener('click',()=>{
-    fetch(`/tweets/all`).then(response => response.json()).then(tweets => {
-        // Print email
-        console.log(tweets)
-    })
+document.querySelector('#all_posts').addEventListener('click', ()=>{
+
+    const el = document.querySelector('#all_posts')
+    console.log(el.dataset.post)
+
+    all_tweets(fetch(`/tweets/all`).then(response => response.json()).then(tweets => {
+            // Print email
+            console.log(tweets)
+        }))
 })
 
 
@@ -44,14 +59,54 @@ document.querySelector('#post').addEventListener('click',()=>{
 
 
 document.querySelector('#following').addEventListener('click',()=>{
-    fetch(`/tweets/following`).then(response => response.json()).then(tweets => {
+    following_tweets(fetch(`/tweets/following`).then(response => response.json()).then(tweets => {
         // Print email
         console.log(tweets)
     })
+)})
+
+
+all_tweets(fetch(`/tweets/all`).then(response => response.json()).then(tweets => {
+    // Print email
+    console.log(tweets)
+}))
 })
 
 
+function compose_tweet() {
+    // Show compose view and hide other views
+    document.querySelector('#user_profile_view').style.display = 'none';
+    document.querySelector('#all_tweets_view').style.display = 'none';
+    document.querySelector('#following_posts_view').style.display = 'none';
+    document.querySelector('#compose-view').style.display = 'block';
 
+}
 
+function all_tweets() {
+    // Show compose view and hide other views
+    document.querySelector('#user_profile_view').style.display = 'none';
+    document.querySelector('#all_tweets_view').style.display = 'block';
+    document.querySelector('#following_tweets_view').style.display = 'none';
+    document.querySelector('#compose-view').style.display = 'block';
 
-})
+}
+
+function following_tweets() {
+    // Show compose view and hide other views
+    document.querySelector('#user_profile_view').style.display = 'none';
+    document.querySelector('#all_tweets_view').style.display = 'none';
+    document.querySelector('#following_tweets_view').style.display = 'block';
+    document.querySelector('#compose-view').style.display = 'none';
+
+}
+
+function user_profile() {
+    // Show compose view and hide other views
+    document.querySelector('#user_profile_view').style.display = 'block';
+    document.querySelector('#all_tweets_view').style.display = 'none';
+    document.querySelector('#following_tweets_view').style.display = 'none';
+    document.querySelector('#compose-view').style.display = 'none';
+
+   
+
+}
