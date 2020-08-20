0bf77b358c86d77e530004c6633cfb487cc36316
Nyasha Vision <nmawungwe@gmail.com>
2020-08-20 08:29:32 +0200

    Liking still not full functional I need to add another API route

--- network/static/network/social.js
+++ network/static/network/social.js
@@ -130,24 +130,55 @@ all_tweets(fetch(`/tweets/all`).then(response => response.json()).then(tweets =>
 
             const user = JSON.parse(document.getElementById('user_id').textContent);
             
-            function userjoin(ids) {
-                return `${ids}`
-            }
+            // function userjoin(ids) {
+            //     return `${ids}`
+            // }
 
-            var ids_ar = [likers_id_ar.map(userjoin)]
-            console.log(ids_ar)
+            // var ids_ar = [likers_id_ar.map(userjoin)]
+            
+
+            // console.log(ids_ar)
 
 
 
-            if (ids_ar.includes(user)) {
-                console.log("Wadii")
+
+
+          fetch(`/tweet/${tweet_id}`).then(response => response.json()).then(tweet => {
+            if (tweet.user_likes_ids.includes(5)) {
+                console.log("wadii")
+                likes = likes-1
+                console.log(tweet_id)
+                fetch(`/tweet/${tweet_id}`, 
+                {
+                    method: 'POST',
+                    body: likes
+                    }).then(response => response.json()).then(result => {
+                        // Print result
+                        console.log(result);
+
+
+
+                        all_tweets(fetch(`/tweets/all`).then(response => response.json()).then(tweets => {
+                            // Print email
+                            // console.log(tweets)
+                            // console.log(tweets[3])
+                    
+                            var messages = tweets.map(label).join(' ')
+                            document.querySelector('#all_tweets_list').innerHTML = messages
+                    
+                            function label(tweet) {
+                    
+                                let time = new Date(tweet.time)
+                                // console.log(time.toDateString())
+                                let date =  time.toDateString().split(' ').slice(1).join(' ') + ", " + time.toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3")
+                                // https://stackoverflow.com/questions/2914443/how-to-hold-three-different-text-alignments-in-one-css-box
+                                    return `<button class="tweet btn btn-light bd btn-block" data-id="${tweet.user_id}"><a><b>${tweet.user_username}</b></a><br>${tweet.body}<br><div>${date}<br><form class="like_form">
+                                    <a href="#" type="submit" class="like" data-id="${tweet.id}" data-likes="${tweet.likes}"/><span class="glyphicon glyphicon-heart"></span></a><div class="like_count">${tweet.likes}</div></form></div></button><br>`
+                            }}))
+                    })    
+                    return false  
             } else {
                 console.log("wadzva hahahaha")
-            }
-    
-
-                
-
                 likes = likes++
                 console.log(tweet_id)
                 fetch(`/tweet/${tweet_id}`, 
@@ -178,7 +209,9 @@ all_tweets(fetch(`/tweets/all`).then(response => response.json()).then(tweets =>
                                     <a href="#" type="submit" class="like" data-id="${tweet.id}" data-likes="${tweet.likes}"/><span class="glyphicon glyphicon-heart"></span></a><div class="like_count">${tweet.likes}</div></form></div></button><br>`
                             }}))
                     })    
-                    return false
+                    return false  
+            }
+          })
         }})
 
 
