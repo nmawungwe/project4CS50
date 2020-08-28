4ef402094e230c616346aec8aea4ce2744654bab
Nyasha Vision <nmawungwe@gmail.com>
2020-08-28 16:33:26 +0200

    added liking functionality but there is room for improvement with react_component

--- network/static/network/logged.js
+++ network/static/network/logged.js
@@ -16,7 +16,41 @@ document.querySelector('form').onsubmit = function() {
             all_tweets(location.reload())
         })
         return false;
-}})
+}
+
+document.querySelectorAll('.like').forEach(button=>{
+    button.onclick = function() {
+        tweet_id = this.dataset.id
+        const user_id = JSON.parse(document.getElementById('user_id').textContent);
+        // console.log(tweet_id)
+
+        fetch(`/tweet/${tweet_id}`).then(response => response.json()).then(tweet => {
+            // console.log(tweet)
+            if (tweet.user_likes_ids.includes(user_id)) {
+                console.log("you shouldn't like")
+                fetch(`/like/${tweet_id}`, 
+                {
+                    method: 'DELETE',
+                    }).then(response => response.json()).then(result => {
+                        // Print result
+                        console.log(result);
+                        all_tweets(location.reload())
+
+                    })
+            } else {
+                fetch(`/like/${tweet_id}`, 
+                    {
+                        method: 'POST'
+                        }).then(response => response.json()).then(result => {
+                            // Print result
+                            console.log(result)
+                            all_tweets(location.reload())
+                        })
+                        return false;  
+            }
+        })
+    }})
+})
 
 
 
