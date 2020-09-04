392b1e5865311ddeb70d38764429eb6821c24b53
Nyasha Vision <nmawungwe@gmail.com>
2020-09-04 19:47:09 +0200

    modified and removed console comments

--- network/static/network/followed.js
+++ network/static/network/followed.js
@@ -1,45 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-// document.querySelectorAll('.like').forEach(button=>{
-//     button.onclick = function() {
-//         tweet_id = this.dataset.id
-//         const user_id = JSON.parse(document.getElementById('user_id').textContent);
-//         let csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
-//         fetch(`/tweet/${tweet_id}`).then(response => response.json()).then(tweet => {
-//             if (tweet.user_likes_ids.includes(user_id)) {
-//                 let request = new Request(
-//                     `/like/${tweet_id}`, 
-//                     {headers: {'X-CSRFToken': csrftoken}})
-//                     fetch(request, {
-//                     method: 'DELETE',
-//                     }).then(response => response.json()).then(result => {
-//                         // Print result
-//                         // console.log(result);
-//                         all_tweets(location.reload())
-//                     })
-//             } else {
-//                 let request = new Request(
-//                 `/like/${tweet_id}`, 
-//                 {headers: {'X-CSRFToken': csrftoken}}
-//                 )
-//                 fetch(request, {
-//                         method: 'POST'
-//                         }).then(response => response.json()).then(result => {
-//                             // Print result
-//                             // console.log(result)
-//                             all_tweets(location.reload())
-//                         })
-//                         return false;  
-//             }
-//         })
-//     }})
-
-
-
-
-
-
-
 like = document.querySelectorAll(".liked");
 like.forEach((element) => {
     like_handeler(element);
@@ -79,11 +39,6 @@ function like_handeler(element) {
         });
     });
   }
-
-
-
-
-
 })
 
 function all_tweets() {
